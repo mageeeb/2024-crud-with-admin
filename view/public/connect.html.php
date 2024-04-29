9ce85b189f550b7f35f49ba8ffb287aabc4ee879
Magib <112185392+mageeeb@users.noreply.github.com>
2024-04-29 10:08:01 +0200

    29_4

--- view/public/connect.html.php
+++ view/public/connect.html.php
@@ -1,25 +1,90 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="/assets/css/fontawesome.css">
+    <link rel="stylesheet" href="/assets/css/templatemo-574-mexant.css">
+    <link rel="stylesheet" href="/assets/css/owl.css">
+    <link rel="stylesheet" href="/assets/css/animate.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
+    <!-- ma CSS -->
+    <link rel="stylesheet" href="css/carteJSON.css">
     <title>Connexion</title>
 </head>
+
 <body>
-    <h1>Connexion</h1>
-    <nav>
-        <ul>
-            <li><a href="./">Accueil</a></li>
-            <li><a href="?connect">Connexion</a>
-        </ul>
-    </nav>
-    <form action="" method="POST" name="connexion">
-        <input type="text" name="username" placeholder="Votre login" required><br>
-        <input type="password" name="passwd" placeholder="Votre mot de passe" required><br>
-        <input type="submit" value="connexion">
+    <section class="contact-us-form">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="section-heading">
+                    <nav aria-label="breadcrumb">
+                <ol class="breadcrumb justify-content-center mb-0 animated slideInDown">
+                    <li class="breadcrumb-item"><a class="text-white" href="./">Home</a></li>
+                    <li class="breadcrumb-item"><a class="text-white" href="?connect">Pages</a></li>
+                </ol>
+            </nav>
+            <div class="page-heading">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="header-text">
+                        <h2>Homepage</h2>
+                        <div class="div-dec"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+                    </div>
+                </div>
+               
+                <div class="col-lg-10 offset-lg-1">
+                    <form action="" method="POST" name="connexion" id="contact">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <fieldset>
+                                    <input type="name" name="username" id="name" placeholder="Votre login" autocomplete="on" required>
+                                </fieldset>
+                            </div>
+                            <div class="col-lg-6">
+                                <fieldset>
+                                    <input type="password" name="passwd" placeholder="Votre mot de passe" autocomplete="on" required><br>
+                                </fieldset>
+                            </div>
+
+                            <div class="col-lg-12">
+                                <fieldset>
+                                    <button type="submit" id="form-submit" class="orange-button float-end" value="connexion">Send Message</button>
+                                </fieldset>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
     </form>
-    <?php
-    // var_dump($_POST);
-    ?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="assets/js/isotope.min.js"></script>
+    <script src="assets/js/owl-carousel.js"></script>
+
+    <script src="/assets/js/tabs.js"></script>
+    <script src="/assets/js/swiper.js"></script>
+    <script src="/assets/js/custom.js"></script>
+    <script src="/assets/style.js"></script>
 </body>
+
 </html>
